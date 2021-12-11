f0a1473fefb2b81b82cdb2db5a603aa67ae8e1a7
Manuel Ernesto <alemdadox@gmail.com>
2021-12-11 13:07:48 +0100

    setup customer service

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+9
+	.dcignore,7/7/7791000a9a93019a87d67e0b676c86447ba29c67
---
+++ customer/src/main/java/io/github/manuelernesto/customer/CustomerApplication.java
@@ -0,0 +1,16 @@
+package io.github.manuelernesto.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Manuel Ernesto (manuelernest0)
+ * @version 1.0
+ * @date 11/12/21 13:00
+ */
+@SpringBootApplication
+public class CustomerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerApplication.class, args);
+    }
+}
