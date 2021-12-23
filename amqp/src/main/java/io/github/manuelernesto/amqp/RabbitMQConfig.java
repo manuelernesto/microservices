5e6599a1d5e6d3f80debac9225ea13ee4cdb73a0
Manuel Ernesto <alemdadox@gmail.com>
2021-12-23 22:55:41 +0100

    add rabbitmq config

--- amqp/src/main/java/io/github/manuelernesto/amqp/RabbitMQConfig.java
+++ amqp/src/main/java/io/github/manuelernesto/amqp/RabbitMQConfig.java
@@ -2,6 +2,7 @@ package io.github.manuelernesto.amqp;
 
 import lombok.AllArgsConstructor;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -31,5 +32,13 @@ public class RabbitMQConfig {
     @Bean
     public MessageConverter jacksonConverter() {
         return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory simpleRabbitListenerContainerFactory() {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(jacksonConverter());
+        return factory;
     }
 }
